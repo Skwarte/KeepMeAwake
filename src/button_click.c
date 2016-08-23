384e918e24f38f98566c9313a5f5209610705791
Samuel Kwarteng <skwarte@purdue.edu>
2016-08-23 12:51:06 -0500

    figured out vibration/ got text layer working(ish)

--- src/button_click.c
+++ src/button_click.c
@@ -2,9 +2,13 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static TextLayer *interval;
+
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
+  
+
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -13,6 +17,7 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Down");
+  vibes_short_pulse();
 }
 
 static void click_config_provider(void *context) {
@@ -23,12 +28,19 @@ static void click_config_provider(void *context) {
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+//  GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+ // text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 100));
+  interval = text_layer_create(GRect(0, 0, 0, 400));
+  text_layer_set_text(interval, "Set time interval");
+  text_layer_set_font(interval, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_background_color(interval, GColorBlack);
+ // text_layer_set_text(text_layer, "Press a button");
+ // text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(interval, GTextAlignmentCenter);
+  text_layer_set_size(interval, GSize(100, 40));
+  layer_add_child(window_layer, text_layer_get_layer(interval));
+  //layer_insert_below_sibling(text_layer_get_layer(interval), text_layer_get_layer(text_layer));
 }
 
 static void window_unload(Window *window) {
